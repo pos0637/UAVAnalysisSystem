8fd97c839b9fba172e5c361492794c72e6a690a7
root <root@DESKTOP-B589V5B.localdomain>
2020-08-07 18:11:15 +0800

    添加无人机数据查询界面。

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,79 +1,111 @@
 <template>
-    <baidu-map ref="bmap" class="map" ak="xYQGkcrw9Rd7V7mW8hWa4oSwXhIy9dx8" center="上海">
-        <bm-map-type
-            :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']"
-            anchor="BMAP_ANCHOR_TOP_LEFT"
-        ></bm-map-type>
-        <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
-        <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
-        <bm-overview-map anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :isOpen="true"></bm-overview-map>
-        <bm-polyline v-for="(path, index) in lines.paths" :key="index" :path="path"></bm-polyline>
-    </baidu-map>
+    <a-layout class="layout">
+        <a-layout-header>Header</a-layout-header>
+        <a-layout-content>
+            <a-row>
+                <a-form layout="inline" :form="query">
+                    <a-row type="flex" align="middle">
+                        <a-col flex="120px">
+                            <span>无人机数据名称:</span>
+                        </a-col>
+                        <a-col flex="auto">
+                            <a-input placeholder="请输入无人机数据名称" style="width: 100%" />
+                        </a-col>
+                        <a-col flex="100px" style="text-align: center">
+                            <a-button type="primary" html-type="submit">
+                                查询
+                            </a-button>
+                        </a-col>
+                    </a-row>
+                </a-form>
+            </a-row>
+            <a-row>
+                <a-table :columns="columns" :data-source="data">
+                    <a slot="name" slot-scope="text">{{ text }}</a>
+                    <span slot="customTitle">Name</span>
+                    <span slot="tags" slot-scope="tags">
+                        <a-tag v-for="tag in tags" :key="tag" :color="tag === '错误' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'">
+                            {{ tag.toUpperCase() }}
+                        </a-tag>
+                    </span>
+                    <span slot="action" slot-scope="text, record">
+                        <a>Invite 一 {{ record.name }}</a>
+                        <a-divider type="vertical" />
+                        <a>Delete</a>
+                        <a-divider type="vertical" />
+                        <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+                    </span>
+                </a-table>
+            </a-row>
+        </a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
 </template>
 
 <style scoped>
-.map {
+.layout {
     width: 100%;
-    height: 300px;
+    height: 100%;
 }
 </style>
 
 <script>
-import BaiduMap from 'vue-baidu-map/components/map/Map.vue';
-import { BmMapType, BmScale, BmNavigation, BmOverviewMap, BmPolyline } from 'vue-baidu-map';
+const columns = [
+    {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: '测试地点',
+        dataIndex: 'address',
+        key: 'address'
+    },
+    {
+        title: '测试时间',
+        dataIndex: 'time',
+        key: 'time'
+    },
+    {
+        title: '标签',
+        key: 'tags',
+        dataIndex: 'tags',
+        scopedSlots: { customRender: 'tags' }
+    },
+    {
+        title: '操作',
+        key: 'action',
+        scopedSlots: { customRender: 'action' }
+    }
+];
+
+const data = [
+    {
+        key: '1',
+        name: '浦东国际机场1',
+        address: '浦东国际机场',
+        tags: ['正常', '测试']
+    },
+    {
+        key: '2',
+        name: '浦东国际机场2',
+        address: '浦东国际机场',
+        tags: ['错误']
+    },
+    {
+        key: '3',
+        name: '浦东国际机场3',
+        address: '浦东国际机场',
+        tags: ['正常']
+    }
+];
 
 export default {
-    components: {
-        BaiduMap,
-        BmMapType,
-        BmScale,
-        BmNavigation,
-        BmOverviewMap,
-        BmPolyline
-    },
     data() {
         return {
-            lines: {
-                editing: false,
-                paths: [
-                    [
-                        {
-                            lng: 121.814224,
-                            lat: 31.156484
-                        },
-                        {
-                            lng: 121.808547,
-                            lat: 31.165754
-                        },
-                        {
-                            lng: 121.796186,
-                            lat: 31.184848
-                        },
-                        {
-                            lng: 121.788353,
-                            lat: 31.192015
-                        },
-                        {
-                            lng: 121.771752,
-                            lat: 31.194734
-                        },
-                        {
-                            lng: 121.74969,
-                            lat: 31.189791
-                        },
-                        {
-                            lng: 121.754217,
-                            lat: 31.179658
-                        },
-                        {
-                            lng: 121.757523,
-                            lat: 31.167794
-                        }
-                    ]
-                ]
-            }
+            data,
+            columns
         };
-    },
-    methods: {}
+    }
 };
 </script>
---
+++ ui/src/views/Map.vue
@@ -0,0 +1,117 @@
+<template>
+    <baidu-map ref="bmap" class="map" ak="xYQGkcrw9Rd7V7mW8hWa4oSwXhIy9dx8" center="上海">
+        <bm-map-type :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']" anchor="BMAP_ANCHOR_TOP_LEFT"></bm-map-type>
+        <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+        <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+        <bm-overview-map anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :isOpen="true"></bm-overview-map>
+        <bm-polyline v-for="(path, index) in lines.paths" :key="index" :path="path.points" :stroke-color="path.color" :stroke-opacity="0.5"></bm-polyline>
+    </baidu-map>
+</template>
+
+<style scoped>
+.map {
+    width: 100%;
+    height: 100%;
+}
+</style>
+
+<script>
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue';
+import { BmMapType, BmScale, BmNavigation, BmOverviewMap, BmPolyline } from 'vue-baidu-map';
+
+export default {
+    name: 'Map',
+    components: {
+        BaiduMap,
+        BmMapType,
+        BmScale,
+        BmNavigation,
+        BmOverviewMap,
+        BmPolyline
+    },
+    data() {
+        return {
+            lines: {
+                editing: false,
+                paths: [
+                    {
+                        color: '#00FF00',
+                        points: [
+                            {
+                                lng: 121.814224,
+                                lat: 31.156484
+                            },
+                            {
+                                lng: 121.808547,
+                                lat: 31.165754
+                            },
+                            {
+                                lng: 121.796186,
+                                lat: 31.184848
+                            },
+                            {
+                                lng: 121.788353,
+                                lat: 31.192015
+                            },
+                            {
+                                lng: 121.771752,
+                                lat: 31.194734
+                            },
+                            {
+                                lng: 121.74969,
+                                lat: 31.189791
+                            },
+                            {
+                                lng: 121.754217,
+                                lat: 31.179658
+                            },
+                            {
+                                lng: 121.757523,
+                                lat: 31.167794
+                            }
+                        ]
+                    },
+                    {
+                        color: 'red',
+                        points: [
+                            {
+                                lng: 121.814324,
+                                lat: 31.126484
+                            },
+                            {
+                                lng: 121.808447,
+                                lat: 31.135754
+                            },
+                            {
+                                lng: 121.796286,
+                                lat: 31.174848
+                            },
+                            {
+                                lng: 121.788453,
+                                lat: 31.112015
+                            },
+                            {
+                                lng: 121.771152,
+                                lat: 31.154734
+                            },
+                            {
+                                lng: 121.74569,
+                                lat: 31.179791
+                            },
+                            {
+                                lng: 121.756217,
+                                lat: 31.139658
+                            },
+                            {
+                                lng: 121.752523,
+                                lat: 31.127794
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
+    },
+    methods: {}
+};
+</script>
